149bba09ce53e0b3a682ac375b65e9c94f58200c
Ophus Wong <ophus.wong@gmail.com>
2018-01-02 00:00:14 -0700

    Adjust CSS styling Add breakpoints for Header, Banner, Story, Stories, and Footer components Enable components for importing Author component into story Add Story component to Stories component

--- src/Story.js
+++ src/Story.js
@@ -6,14 +6,17 @@ import Author from './Author';
 const Story = (props) => {
   return (
     <div className='Story'>
-      <img src={props.img} />
+      <div className='story-img'>
+        <img src={props.img} />
+      </div>
       <div className='story-info'>
         <div>
           <h3>{props.title}</h3>
           <p>{props.desc}</p>
         </div>
-        {/* create an Author component and add it here */}
-        {/* it will need properties of the author image, name, and the estimated reading time of the story */}
+        <Author authImg={props.authImg}
+                authName={props.authName}
+                estTime={props.estTime}/>
       </div>
     </div>
   )
