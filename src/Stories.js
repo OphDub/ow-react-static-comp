edc7c7ae8231dcbb75d7b938d87df1634439ee4b
Ophus Wong <ophus.wong@gmail.com>
2018-01-02 21:02:17 -0700

    Delete notes in Stories component

--- src/Stories.js
+++ src/Stories.js
@@ -5,16 +5,13 @@ import './Stories.css';
 const Stories = (props) => {
   return (
     <div className='Stories'>
-      <div className='  stories-header'>
+      <div className='stories-header'>
         <h3>{props.storiesTitle}</h3>
         <a href='#'>See more</a>
       </div>
       <div className='stories-grid'>
         {
           props.stories.map( (story, index) => {
-            console.log(story);
-            // INSTEAD OF returning the div below, return a Story component
-            // Be sure to pass down the correct props!
             return (<Story  key={index}
                             title={story.title}
                             desc={story.desc}
